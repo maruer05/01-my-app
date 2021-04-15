cbc1f59be118fed10f09b6722e7127821f1eb937
MARIA ROJAS <maru0544@gmail.com>
2021-04-14 21:32:11 -0400

    version de comnicacion websocket

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { webSocket } from "rxjs/webSocket";
 
 @Component({
   selector: 'app-tab1',
@@ -7,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab1Page implements OnInit {
 
-  constructor() {}
+  subject = webSocket('ws://8266esp:81');
+  message = 'hello';
+  
+
+  constructor(
+    
+  ) {}
     
   
   ngOnInit(){
     console.log('hola mundo');
+    this.listentoWebsocket();
+ 
   }
+
+ 
+
+  sendToServer($event){
+    this.subject.subscribe();
+    this.subject.next(this.message);
+    this.subject.complete();
+    this.subject.error({code: 4000, reason: 'I think our app just broke!'});
+  }
+
+  listentoWebsocket(){
+    this.subject.subscribe(
+      msg => console.log('message received: ' + msg), // Called whenever there is a message from the server.
+      err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+      () => console.log('complete') // Called when connection is closed (for whatever reason).
+    );
+  }
+
 }
